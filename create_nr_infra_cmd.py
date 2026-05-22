cf697ce2453561e261deaeb5eb06341c9d347aa0
Shayan Saha <shayan851997@gmail.com>
2026-05-22 11:51:02 +0530

    Command generator takes account ID from config.ini file

--- create_nr_infra_cmd.py
+++ create_nr_infra_cmd.py
@@ -1,9 +1,15 @@
+import configparser
+import os
 
-
-
-def create_infra_cmd(apiKey : str):
+def create_infra_cmd(apiKey: str):
+    config = configparser.ConfigParser()
+    config_path = os.path.join(os.path.dirname(__file__), 'config.ini')
+    config.read(config_path)
+    
+    ACCOUNT_ID = config.get('newrelic', 'account_id')
+    
     cmd = f"""
-    curl -Ls https://download.newrelic.com/install/newrelic-cli/scripts/install.sh | bash && sudo NEW_RELIC_API_KEY={apiKey} NEW_RELIC_ACCOUNT_ID=3774110 /usr/local/bin/newrelic install -y
+    curl -Ls https://download.newrelic.com/install/newrelic-cli/scripts/install.sh | bash && sudo NEW_RELIC_API_KEY={apiKey} NEW_RELIC_ACCOUNT_ID={ACCOUNT_ID} /usr/local/bin/newrelic install -y
     """
 
     return cmd
